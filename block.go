d8ceef615197d87b1c76c0d106868032a6cb4e4f
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-08-31 23:38:43 +0900

    add: block

---
+++ block.go
@@ -0,0 +1,52 @@
+package goblockchain
+
+import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Block struct {
+	nonce        int
+	prevHash     [32]byte
+	timestamp    int64
+	transactions []*Transaction
+}
+
+func NewBlock(nonce int, prevHash [32]byte, transactions []*Transaction) *Block {
+	return &Block{
+		nonce:        nonce,
+		prevHash:     prevHash,
+		timestamp:    time.Now().Unix(),
+		transactions: transactions,
+	}
+}
+
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+	return sha256.Sum256([]byte(m))
+}
+
+func (b *Block) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		TimeStamp    int64          `json:"timestamp"`
+		Nonce        int            `json:"nonce"`
+		PrevHash     [32]byte       `json:"previous_hash"`
+		Transactions []*Transaction `json:"transactions"`
+	}{
+		TimeStamp:    b.timestamp,
+		Nonce:        b.nonce,
+		PrevHash:     b.prevHash,
+		Transactions: b.transactions,
+	})
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp        %d\n", b.timestamp)
+	fmt.Printf("nonce            %d\n", b.nonce)
+	fmt.Printf("previous_hash    %s\n", b.prevHash)
+	for _, t := range b.transactions {
+		t.Print()
+	}
+}
