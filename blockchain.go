e77b2d18a09719b03a9ca3b1dbf1d17c8b761343
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-08-31 23:42:55 +0900

    add: blockchain

---
+++ blockchain.go
@@ -0,0 +1,43 @@
+package goblockchain
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+type Blockchain struct {
+	transactionPool []*Transaction
+	chain           []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	b := &Block{}
+	bc := new(Blockchain)
+	bc.CreateBlock(0, b.Hash())
+	return bc
+}
+
+func (bc *Blockchain) CreateBlock(nonce int, prevHash [32]byte) *Block {
+	b := NewBlock(nonce, prevHash, bc.transactionPool)
+	bc.chain = append(bc.chain, b)
+	bc.transactionPool = []*Transaction{}
+	return b
+}
+
+func (bc *Blockchain) LastBlock() *Block {
+	return bc.chain[len(bc.chain)-1]
+}
+
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Println(strings.Repeat("*", 25))
+}
+
+func (bc *Blockchain) AddTransaction(sender, recipient string, value float32) {
+	t := NewTransaction(sender, recipient, value)
+	bc.transactionPool = append(bc.transactionPool, t)
+}
